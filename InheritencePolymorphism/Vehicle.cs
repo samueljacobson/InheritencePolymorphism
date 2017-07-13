0b42375b184e7ecbe88c05c67a9acc86678558b1
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-13 10:29:36 -0400

    Added base class

---
+++ InheritencePolymorphism/Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritencePolymorphism
+{
+    class Vehicle
+    {
+        //Fields
+        protected int seats;
+        protected int carryingCapacity;
+        protected string color;
+        protected double movementSpeed;
+        protected double distanceTraveled;
+
+        private float gearRatio;
+        private float engineSize;
+
+        //Properties
+        public int Seats { get; }
+        public int CarryCapacity { get; }
+        public string Color { get; set; }
+        public double DistanceTraveled { get; set; }
+
+        //Constructors
+        public Vehicle()
+        {
+
+        }
+
+        public Vehicle(int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+
+            distanceTraveled = 0.0d;
+        }
+
+        //Methods
+        public virtual void Move()
+        {
+            distanceTraveled += movementSpeed;
+        }
+
+        public virtual string GetDistanceTraveled()
+        {
+            return "Distance Traveled: " + distanceTraveled + "\n";
+        }
+    }
+}
